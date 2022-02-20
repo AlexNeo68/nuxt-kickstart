011edf06cc29bddaa3700390f192976bf30b3cc3
Александр Неудахин <aleksandrneudahin@Aleksandrs-Air.localnet>
2022-02-20 08:18:27 +0300

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   head: {
-    title: "nuxt-kickstart",
+    title: "CryptoCrowd",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
